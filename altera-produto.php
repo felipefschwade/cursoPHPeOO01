4e68832ece462d636ba45d14c02e62f31c30d693
Felipe F. Schwade <felipefaeschwade@gmail.com>
2016-11-08 14:37:39 -0400

    Adicionando a categoria à listagem

--- altera-produto.php
+++ altera-produto.php
@@ -2,6 +2,7 @@
 require_once("cabecalho.php");
 require_once("banco-produto.php");
 require_once("class/Produto.php");
+require_once("class/Categoria.php");
 
 $Produto = new Produto();
 $categoria = new Categoria();
--- banco-categoria.php
+++ banco-categoria.php
@@ -7,7 +7,10 @@ function listaCategorias($conexao) {
 	$query = "select * from categorias";
 	$resultado = mysqli_query($conexao, $query);
 
-	while($categoria = mysqli_fetch_assoc($resultado)) {
+	while($categoria_array = mysqli_fetch_assoc($resultado)) {
+		$categoria = new Categoria();
+		$categoria->id = $categoria_array['id'];
+		$categoria->nome = $categoria_array['nome'];
 		array_push($categorias, $categoria);
 	}
 
